31dec64851edaf893d5e029176b4f8385b2d3a71
sua2023 <thong.99485942@gmail.com>
2025-10-24 19:55:06 +0700

    auth

--- src/middleware/handlemiddleware.ts
+++ src/middleware/handlemiddleware.ts
@@ -3,13 +3,13 @@ import { redirect } from "next/navigation";
 export default async function HandleMiddleware(role: string[]) {
   const cookieStore = cookies();
   const role_cookie: any = cookieStore.get("role")?.value;
-  // if (role_cookie) {
-  //   if (role.includes(role_cookie)) { 
-  //     return true;
-  //   } else {
-  //     redirect("/login");
-  //   }
-  // } else {
-  //   redirect("/login");
-  // }
+  if (role_cookie) {
+    if (role.includes(role_cookie)) { 
+      return true;
+    } else {
+      redirect("/login");
+    }
+  } else {
+    redirect("/login");
+  }
 }
